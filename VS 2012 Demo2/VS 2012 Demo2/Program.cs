db1e0aab793cb9ef83614eba39c822e2f5d970f7
Matthew Sponar <msponar@subcom.com>
2016-04-09 15:53:37 -0400

    Initial project

---
+++ VS 2012 Demo2/VS 2012 Demo2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VS_2012_Demo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
